a337019aa56fd972038312fddbc58cb2c79c8a5b
Itz_jeslin <124077022+jeslin612@users.noreply.github.com>
2025-07-16 19:28:41 +0530

    Add files via upload

---
+++ if.js
@@ -0,0 +1,13 @@
+let a=10;
+if(a%5==0)
+{
+    console.log("Given value is Divisible by 5")
+}
+if(a%2==0)
+{
+    console.log("Even Number")
+}
+else
+{
+    console.log("Odd Number")
+}
---
+++ loop2.js
@@ -0,0 +1,19 @@
+for (let i=10; i>=1; i--)
+{
+    console.log(i);
+}
+let j=10;
+while(j>=1)
+{
+    console.log(j);
+    j--;
+}
+let k=10;
+do
+{
+    console.log(k);
+    k--;
+
+}while(k>=1);
+
+
